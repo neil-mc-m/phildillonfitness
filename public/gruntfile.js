2d025352f12f6774e5b31ec92ae2ea6477f33be5
studentneil <neilo2000@gmail.com>
2017-10-22 21:47:12 +0100

    added the template and gruntfile

---
+++ public/gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        autoprefixer: {
+            options: {
+                browsers: ['> 5%', 'last 2 versions', 'ie 11']
+            },
+            dist: {
+                files: {
+                    'build/css/prefix-main.css': 'css/main.css'
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['css/main.css'],
+                tasks: ['autoprefixer']
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: ["css/main.css",
+                        "/resources/views/*.twig",
+                        // "../templates/backend/*.html.twig",
+                        // "../templates/frontend/partials/*.html.twig",
+                        // "../templates/backend/partials/*.html.twig"
+                    ]
+                },
+                options: {
+                    proxy: "http://localhost:8888",
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.registerTask('default', ['browserSync']);
+};
